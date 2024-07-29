e9b501dcbdcdcfc218036d1b3511597ca7c38494
ZahaabAlim <zahaabalim@gmail.com>
2024-07-29 14:13:57 +0530

    avsdf

--- main.tf
+++ main.tf
@@ -2,20 +2,26 @@ terraform {
   required_providers {
     spotify = {
       source  = "conradludgate/spotify"
-      version = "0.1.1"
+      version = "0.2.7"
     }
   }
 }
 
 provider "spotify" {
-  oauth_client_id     = var.spotify_client_id
-  oauth_client_secret = var.spotify_client_secret
-  redirect_uri        = var.spotify_redirect_uri
-  access_token        = var.spotify_access_token
+  client_id     = var.spotify_client_id
+  client_secret = var.spotify_client_secret
+  redirect_uri  = "http://localhost:27228/spotify_callback"
 }
 
 resource "spotify_playlist" "my_playlist" {
   name        = "My Terraform Playlist"
-  description = "A playlist created with Terraform"
+  description = "A playlist created using Terraform"
   public      = true
+}
+resource "spotify_playlist_tracks" "my_playlist_tracks" {
+  playlist_id = spotify_playlist.my_playlist.id
+  tracks      = [
+    "spotify:track:3I3kZyHUtEA9Y59rJkxtk6",  # Example track URIs
+    "spotify:track:1301WleyT98MSxVHPZCA6M"
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,9 @@
-
 variable "spotify_client_id" {
   description = "Spotify Client ID"
+  type        = string
 }
 
 variable "spotify_client_secret" {
   description = "Spotify Client Secret"
-}
-
-variable "spotify_redirect_uri" {
-  description = "Spotify Redirect URI"
-}
-
-variable "spotify_access_token" {
-  description = "Spotify Access Token"
+  type        = string
 }
